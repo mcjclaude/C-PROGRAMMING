57594db7beba06b3954a0493484ca9a6ed3ecfa0
mcjclaude <mcjclaude2002@gmail.com>
2024-01-15 12:28:38 +0300

    CHANGES

--- degree.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int Fahr;
-for(Fahr = 0; Fahr <= 300; Fahr = Fahr + 20 );
-     printf("%3d %6.1f\n" , Fahr , (5.0/9.0)*(Fahr-32) );
-     
-}
-
--- tabs.c
+++ tabs.c
@@ -4,13 +4,8 @@ int main ()
 {
     int c , nl;
      nl = 0;
-     int bb = 0;
     while ((c =getchar()) != EOF)
         if (c == ' ')
             ++nl;
-            printf("%d blanks\n", nl); 
-
-            if ( c == '\t')   
-                 ++bb;
-                 printf("%d tabs\n");
+            printf("%d blanks\n", nl);  
 }
